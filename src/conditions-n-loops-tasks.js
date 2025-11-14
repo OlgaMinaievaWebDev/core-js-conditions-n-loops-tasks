e4d8246435d4aa12db5256f4f244f8bf474b18d0
Olga <minaeva9@gmail.com>
2025-11-14 15:09:22 -0500

    feat:few tasks solved

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -295,8 +295,16 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let current = num;
+  while (current > 0) {
+    const remainder = current % 10;
+    if (remainder === digit) {
+      return true;
+    }
+    current = Math.floor(current / 10);
+  }
+  return false;
 }
 
 /**
@@ -312,8 +320,23 @@ function isContainNumber(/* num, digit */) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  let totalSum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    totalSum += arr[i];
+  }
+
+  let leftSum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    const rightSum = totalSum - leftSum - arr[i];
+
+    if (leftSum === rightSum) {
+      return i;
+    }
+
+    leftSum += arr[i];
+  }
+  return -1;
 }
 
 /**
